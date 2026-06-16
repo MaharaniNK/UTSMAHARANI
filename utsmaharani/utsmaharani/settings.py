c53172197d0bfaa48fefd752528ed40bc334b1cc
MaharaniNK <maharani2367@gmail.com>
2026-06-16 20:17:14 +0700

    Prepare Django for deployment

--- utsmaharani/utsmaharani/settings.py
+++ utsmaharani/utsmaharani/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8+jwhfz_#19-g4s)-5@2e=iesc1k-bk#(yyy8v0yrmu+-)mjg)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
